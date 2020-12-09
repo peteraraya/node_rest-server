e8ef2c4877f8e974b540154cf84c3ff557e87e67
“Pedro” <paraya@ticblue.com>
2020-12-09 13:13:02 -0300

    cambios nuevos

--- server/config/config.js
+++ server/config/config.js
@@ -5,22 +5,22 @@
 // =================
 // Puerto 
 // =================
-process.env.PORT = process.env.PORT || 3000
+process.env.PORT = process.env.PORT || 3000;
 
 // =================
 // Entorno 
 // =================
-process.env.NODE_ENV = process.env.NODE_ENV || "dev"
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 // =================
 // Base de datos 
 // =================
 let urlDB;
 
-if ( process.env.NODE_ENV === "dev") {
+if ( process.env.NODE_ENV === 'dev') {
     urlDB = "mongodb://localhost:27017/cafe";
 }else{
-    urlDB = "mongodb+srv://peternt:kprKrVM8Lnro89eo@cluster0.pvdzf.mongodb.net/cafe?retryWrites=true&w=majority";
+    urlDB = "mongodb+srv://peternt:<kprKrVM8Lnro89eo>@cluster0.pvdzf.mongodb.net/cafe";
 }
 process.env.URLDB = urlDB;
 
