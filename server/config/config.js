07dc62d251e9bec6e62d357889105d3cd0cf9159
“Pedro” <paraya@ticblue.com>
2020-12-09 13:28:34 -0300

    ultimos cambios

--- server/config/config.js
+++ server/config/config.js
@@ -1,26 +1,25 @@
 
 // process : es un objeto global que esta corriendo por toda la app de node
 
-
-// =================
-// Puerto 
-// =================
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
 
-// =================
-// Entorno 
-// =================
-process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
-// =================
-// Base de datos 
-// =================
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============================
+//  Base de datos
+// ============================
 let urlDB;
 
-if ( process.env.NODE_ENV === 'dev') {
-    urlDB = "mongodb://localhost:27017/cafe";
-}else{
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
     urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
-
